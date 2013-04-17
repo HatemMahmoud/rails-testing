d9c8c59313417ab4f988e97af0455837faa5656e
Hatem Mahmoud <hatem@expressionlab.com>
2013-04-17 18:27:53 +0200

    Added capybara and launchy to "Integration Testing"

--- tasks/test/integration/task_management_test.rb
+++ tasks/test/integration/task_management_test.rb
@@ -7,4 +7,14 @@ class TaskManagementTest < ActionDispatch::IntegrationTest
     # assert_response :success
     # assert_select 'li.task', task.name
   # end
+  
+  test 'creates a new task' do
+    visit tasks_path
+    click_link 'New Task'
+    fill_in 'task_name', with: 'Learn TDD'
+    click_button 'Create Task'
+    save_and_open_page
+    assert page.has_content? 'Task was successfully created.'
+    assert page.has_content? 'Learn TDD'
+  end
 end
