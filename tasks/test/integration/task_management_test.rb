be5fc09760023962beeb5d35eb4e1048c5588aa4
Hatem Mahmoud <hatem@expressionlab.com>
2013-04-17 17:09:36 +0200

    Added "Factories" section

--- tasks/test/integration/task_management_test.rb
+++ tasks/test/integration/task_management_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class TaskManagementTest < ActionDispatch::IntegrationTest
-  test 'list all tasks' do
-    task = tasks(:milk)
-    get tasks_path
-    assert_response :success
-    assert_select 'li.task', task.name
-  end
+  # test 'list all tasks' do
+    # task = tasks(:milk)
+    # get tasks_path
+    # assert_response :success
+    # assert_select 'li.task', task.name
+  # end
 end
