cadaa4cd4537c4753be9a4614d4f4058bcb8a647
Hatem Mahmoud <hatem@expressionlab.com>
2013-04-18 12:58:54 +0200

    rails new mooc  --skip-test-unit

---
+++ mooc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mooc::Application.config.secret_token = '8c1f5c5b92f20272657b18bbb793bb99619f2759f2c1e249c95c1eb651c416123e0da6b172cecd123985823938d438c478711f2d6e4e724890e1e18dd4cf3175'
